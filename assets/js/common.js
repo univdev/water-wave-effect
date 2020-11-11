cd744cc5a972603a8e45f5953678eb592ad099e9
박찬영 <chanyoung@starshell.co>
2020-11-11 21:34:48 +0900

    포인트 애니메이션 구현

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,29 +1,32 @@
 class App {
-  constructor() {
+  constructor() {    
     this.canvas = document.createElement('canvas');
     this.canvas.setAttribute('id', 'canvas');
     this.ctx = this.canvas.getContext('2d');
-    document.body.prepend(this.canvas); // 캔버스 배치
     this.resize();
 
-    window.addEventListener('resize', () => {
-      this.resize();
-    });
+    const startX = 0;
+    const endX = this.screenWidth;
+    const centerY = this.screenCenterY;
+    const points = new Points(startX, endX, centerY, 5);
+    this.points = points;
+    this.fps = 60;
 
-    // window.setInterval(() => {
-    //   this.clear();
-    //   this.draw();
-    // }, 10);
+    document.body.prepend(this.canvas); // 캔버스 배치
+
+    window.setInterval(() => {
+      this.draw();
+    }, 1000 / this.fps);
   }
 
   resize() {
     this.screenWidth = window.document.documentElement.clientWidth;
     this.screenHeight = window.document.documentElement.clientHeight;
+    this.screenCenterX = this.screenWidth / 2;
+    this.screenCenterY = this.screenHeight / 2;
     this.canvas.setAttribute('width', this.screenWidth * 2);
     this.canvas.setAttribute('height', this.screenHeight * 2);
     this.ctx.scale(2, 2);
-    this.clear();
-    this.draw();
   }
   
   clear() {
@@ -31,12 +34,10 @@ class App {
   }
 
   draw() {
-    const startX = 0;
-    const endX = this.screenWidth;
-    const centerY = this.screenHeight / 2;
-    const points = new Points(startX, endX, centerY, 5);
-    for (let i = 0; i < points.length; i += 1) {
-      const point = points.items[i];
+    this.clear();
+    for (let i = 0; i < this.points.length; i += 1) {
+      const point = this.points.items[i];
+      point.animate();
       this.ctx.beginPath();
       this.ctx.fillStyle = 'red';
       this.ctx.fillRect(point.x, point.y, 5, 5);
@@ -46,24 +47,22 @@ class App {
 }
 
 class Point {
-  constructor(fixedX, fixedY, distance, speed, currentDirection) {
+  constructor(fixedX, fixedY) {
     // fixedX: 이 점이 가질 고정 x
     // fixedY: 이 점이 가질 고정 y
-    // distance: 고정 높이로부터 움직일 수 있는 거리의 상한선
+    // maxDistance: 고정 높이로부터 움직일 수 있는 거리의 상한선
     // speed: 거리의 상한선 내에서 거리가 조정되는 속도
-    // currentDirection: 포인터가 움직이는 방향을 1(아래)이나 -1(위)로 나타냄.
     this.fixedX = fixedX;
     this.fixedY = fixedY;
     this.x = fixedX;
     this.y = fixedY;
-    this.distance = distance;
-    this.speed = speed;
-    this.currentDirection = currentDirection;
-    this.animate();
+    this.maxDistance = Math.random() * 100 + 150;
+    this.cur = 0;
+    this.speed = 0.1;
   }
   animate() {
-    const destination = this.fixedY + (this.distance * this.currentDirection);
-    console.log(destination);
+    this.cur += this.speed;
+    this.y = this.fixedY + (Math.sin(this.cur) * this.maxDistance);
   }
 }
 
