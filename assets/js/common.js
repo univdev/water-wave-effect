04f917d0bc2e43a6285a281cd01ef95d913c8e07
박찬영 <chanyoung@starshell.co>
2020-11-11 21:56:46 +0900

    웨이브 애니메이션 제작 완료

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,13 +3,14 @@ class App {
     this.canvas = document.createElement('canvas');
     this.canvas.setAttribute('id', 'canvas');
     this.ctx = this.canvas.getContext('2d');
+    this.color = { r: 0, g: 0, b: 255 };
+    this.waves = [];
     this.resize();
 
     const startX = 0;
     const endX = this.screenWidth;
     const centerY = this.screenCenterY;
-    const points = new Points(startX, endX, centerY, 5);
-    this.points = points;
+    for (let i = 0; i < 5; i += 1) this.waves.push(new Points(startX, endX, centerY, 5));
     this.fps = 60;
 
     document.body.prepend(this.canvas); // 캔버스 배치
@@ -35,12 +36,28 @@ class App {
 
   draw() {
     this.clear();
-    for (let i = 0; i < this.points.length; i += 1) {
-      const point = this.points.items[i];
-      point.animate();
+    for (let i = 0; i < this.waves.length; i += 1) {
+      const wave = this.waves[i];
+      const firstPoint = wave.items[0];
+      const opacityDistance = 0.8 / this.waves.length;
+      const opacity = 0.8 - opacityDistance * i;
+      let prevX = firstPoint.x;
+      let prevY = firstPoint.y;
       this.ctx.beginPath();
-      this.ctx.fillStyle = 'red';
-      this.ctx.fillRect(point.x, point.y, 5, 5);
+      this.ctx.fillStyle = `rgba(${this.color.r}, ${this.color.g}, ${this.color.b}, ${opacity})`;
+      this.ctx.moveTo(prevX, prevY);
+      for (let j = 1; j < wave.length - 1; j += 1) {
+        wave.items[j].animate();
+      }
+      for (let j = 1; j < wave.length; j += 1) {
+        const point = wave.items[j];
+        this.ctx.quadraticCurveTo(prevX, prevY, point.x, point.y);
+        prevX = point.x;
+        prevY = point.y;
+      }
+      this.ctx.lineTo(this.screenWidth, this.screenHeight);
+      this.ctx.lineTo(firstPoint.x, this.screenHeight);
+      this.ctx.fill();
       this.ctx.closePath();
     }
   }
@@ -71,14 +88,15 @@ class Points {
     this.items = [];
     const distance = Math.abs(endX - startX);
     const pointDistance = distance / count;
-    for (let i = 0; i < count; i += 1) {
+    for (let i = 0; i < (count - 1); i += 1) {
       const x = pointDistance * (i + 1);
       const y = centerY;
       const centerIndex = count / 2;
       const direction = (centerIndex <= i) ? -1 : 1;
       this.items.push(new Point(x, y, 500, 1, direction));
     }
-    this.items.unshift(new Point(0, centerY, 0, 0, 0));
+    this.items.unshift(new Point(0, centerY));
+    this.items.push(new Point(endX, centerY));
     this.length = this.items.length;
   }
 }
